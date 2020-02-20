1109eec45ddc89aa31bd9935accd74875a831066
Burkun <duno3avr@gmail.com>
2020-02-20 18:31:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+	return +n.toString().slice(1).split('').reverse().join('');
+  }
+  return +n.toString().split('').reverse().join('');
 }
